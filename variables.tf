a136a2189cc36b915e55da4e6082e4be36776ccd
uddithmachiraju <uddithmachiraju@gmail.com>
2025-05-07 07:16:14 +0000

    Env Variables

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "lambda_env" {
-    description = "Indicated if running in Lambda environment"
+    description = "Indicates if running in Lambda environment"
     type = string 
     default = "true" 
 }
