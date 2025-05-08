2b4f65829aa8926a5165a5f8569f0a456df96789
uddithmachiraju <uddithmachiraju@gmail.com>
2025-05-08 12:20:33 +0000

    Updated CORS settings

--- main.tf
+++ main.tf
@@ -62,7 +62,6 @@ resource "aws_lambda_function" "my_lambda" {
   source_code_hash = filebase64sha256("function.zip") # Everytime the "function.zip" is changed automatically deploy it
 }
 
-
 # Creating the REST API 
 resource "aws_api_gateway_rest_api" "rest_api" {
   name        = "NimbusRestApi"
@@ -84,7 +83,15 @@ resource "aws_api_gateway_method" "any_root" {
   authorization = "NONE"
 }
 
-# similar to above allowing ANY HTTP Method 
+# Creating a HTTP method on root path (enabling CORS for our API) 
+resource "aws_api_gateway_method" "options_root" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# similar to above allowing ANY HTTP Method to proxy endpoint 
 resource "aws_api_gateway_method" "any_proxy" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   resource_id   = aws_api_gateway_resource.proxy_resource.id
@@ -92,7 +99,15 @@ resource "aws_api_gateway_method" "any_proxy" {
   authorization = "NONE"
 }
 
-# Linking the lambda with the API we created
+# Creating a HTTP method on proxy path (enabling CORS for our API) 
+resource "aws_api_gateway_method" "options_proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# Linking the lambda with the API we created to root endpoint
 resource "aws_api_gateway_integration" "lambda_root" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_rest_api.rest_api.root_resource_id
@@ -102,7 +117,18 @@ resource "aws_api_gateway_integration" "lambda_root" {
   uri                     = aws_lambda_function.my_lambda.invoke_arn
 }
 
-# similar to above linking the lambda with proxy 
+# Linking the lambda with the API we created to "option"
+resource "aws_api_gateway_integration" "options_root_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_rest_api.rest_api.root_resource_id
+  http_method             = "OPTIONS"
+  type                    = "MOCK"
+  request_templates       = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+# similar to above linking the lambda with proxy endpoint
 resource "aws_api_gateway_integration" "lambda_proxy" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_resource.proxy_resource.id
@@ -110,6 +136,17 @@ resource "aws_api_gateway_integration" "lambda_proxy" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.my_lambda.invoke_arn
+}
+
+# similar to above linking the lambda with options 
+resource "aws_api_gateway_integration" "options_proxy_integration" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = "OPTIONS"
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
 }
 
 # Allowing API Gateway to Invoke(run) Lambda - SAM does this by default
@@ -121,11 +158,79 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
 }
 
+# this below code is to set up CORS (Cross-Origin Resource Sharing) support for both the root (/) and proxy (/{proxy+}) paths of our API Gateway.
+
+# this implements the response returned when an OPTIONS request hits the root (/).
+resource "aws_api_gateway_integration_response" "options_root_response" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_rest_api.rest_api.root_resource_id
+  http_method = aws_api_gateway_method.options_root.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS,PATCH,HEAD'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://prod.d2nr02lclk5abd.amplifyapp.com'"
+  }
+}
+
+# this declares that the root OPTIONS method can return those headers with a 200 OK.
+resource "aws_api_gateway_method_response" "options_root_response" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_rest_api.rest_api.root_resource_id
+  http_method = aws_api_gateway_method.options_root.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+# this mplements the response for the route /{proxy+}. 
+resource "aws_api_gateway_integration_response" "options_proxy_response" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = aws_api_gateway_method.options_proxy.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS,PATCH,HEAD'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://prod.d2nr02lclk5abd.amplifyapp.com'"
+  }
+}
+
+# Declares those headers and the 200 status for the OPTIONS method on the proxy route.
+resource "aws_api_gateway_method_response" "options_proxy_response" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = aws_api_gateway_method.options_proxy.http_method
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
 # Publish/Deploy the API to use for our application
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_integration.lambda_root,
-    aws_api_gateway_integration.lambda_proxy
+    aws_api_gateway_integration.lambda_proxy,
+    aws_api_gateway_integration.options_root_integration,
+    aws_api_gateway_integration.options_proxy_integration
   ]
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   stage_name  = "Prod"
