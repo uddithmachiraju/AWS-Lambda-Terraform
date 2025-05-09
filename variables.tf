b8aa57086382cd051812e6421b026d7ff8fdefc5
uddithmachiraju <uddithmachiraju@gmail.com>
2025-05-09 12:59:50 +0000

    variables required for the architecture

--- variables.tf
+++ variables.tf
@@ -5,17 +5,17 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "lambda_env" {
-    description = "Indicates if running in Lambda environment"
-    type = string 
-    default = "true" 
-}
-
 # S3 Bucket Name
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket to store function.zip"
   type        = string
-  default     = "my-lambda-bucket-sanjay"
+  default     = "my-lambda-bucket-test-27"
+}
+
+variable "lambda_env" {
+    description = "Indicates if running in Lambda environment"
+    type = string 
+    default = "true" 
 }
 
 # Lambda Function Name
@@ -23,4 +23,24 @@ variable "lambda_function_name" {
   description = "The name of the Lambda function"
   type        = string
   default     = "MyFunction"
+}
+
+variable "api_name" {
+  type = string
+  default = "NimbusAPI"
+}
+
+variable "api_description" {
+  type = string
+  default = "Numbus app API gateway" 
+}
+
+variable "allowed_origin" {
+  type    = string
+  default = "'https://prod.d2nr02lclk5abd.amplifyapp.com'"
+}
+
+variable "stage_name" {
+  type    = string
+  default = "Prod" 
 }
