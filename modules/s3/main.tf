2bdf5a2207619d488562c69c6b2bfa46c119b0ee
uddithmachiraju <uddithmachiraju@gmail.com>
2025-05-12 12:45:48 +0000

    Removed s3 bucket creation

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -3,6 +3,10 @@ resource "aws_s3_bucket" "lambda_s3_bucket" {
   bucket = var.s3_bucket_name
 }
 
+# data "aws_s3_bucket" "lambda_s3_bucket" {
+#   bucket = var.s3_bucket_name
+# }
+
 # Upload our 'function.zip' in the s3 bucket we created
 resource "aws_s3_object" "upload_zip" {
   bucket = aws_s3_bucket.lambda_s3_bucket.id
